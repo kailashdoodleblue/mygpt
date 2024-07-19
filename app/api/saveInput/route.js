753a0151dccb351069970e079391c0c82ff0f76c
Kailash <kailash.doodleblue@gmail.com>
2024-07-19 10:54:14 +0530

    second commit

--- app/api/saveInput/route.js
+++ app/api/saveInput/route.js
@@ -1,17 +1,14 @@
 // app/api/saveInput/route.js
 import { NextResponse } from 'next/server';
 import fs from 'fs';
-import path from 'path';
+// import path from 'path';
 
 export async function POST(request) {
   const { input } = await request.json();
 
-  // Define the file path
-  const filePath = path.join(process.cwd(), 'public', 'output.txt');
-
   try {
     // Write the input to the file
-    fs.appendFileSync(filePath,"---"+ input, 'utf8');
+    fs.appendFileSync("public/output.txt","---"+ input, 'utf8');
     return NextResponse.json({ message: 'Input saved successfully' }, { status: 200 });
   } catch (error) {
     console.error(error);
